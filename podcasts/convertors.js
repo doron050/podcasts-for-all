d394a9960419a63c43f7379661fcbf8048c51942
NM <50276767+NivM1@users.noreply.github.com>
2019-05-11 04:32:15 +0300

    using empty genres for extra data on catlog

--- podcasts/convertors.js
+++ podcasts/convertors.js
@@ -4,7 +4,7 @@ const podcastsData = require("./podcastsData");
 const genresData = require("./genresData");
 
 // All functions that convert podcast or episode object to Stremio object
-function episodeToVideo(episode, episodeNumber){
+function episodeToVideo(episode, episodeNumber) {
 
     logger.trace(constants.LOG_MESSAGES.START_CONVERT_EPISODE_TO_VIDEO + episode.id);
 
@@ -26,7 +26,7 @@ function episodeToVideo(episode, episodeNumber){
     return (video);
 }
 
-function episodesToVideos(episodes){
+function episodesToVideos(episodes) {
 
     logger.trace(constants.LOG_MESSAGES.START_CONVERT_EPISODES_TO_VIDEOS + episodes.length);
 
@@ -38,7 +38,7 @@ function episodesToVideos(episodes){
     let episodeNumber = 1;
 
     episodes.forEach(episode => {
-        
+
         let currentVideo = episodeToVideo(episode, episodeNumber);
 
         episodeNumber++;
@@ -50,37 +50,38 @@ function episodesToVideos(episodes){
     return (videos);
 }
 
-function podcastToSeries(podcast){
+function podcastToSeries(podcast) {
 
     logger.trace(constants.LOG_MESSAGES.START_CONVERT_PODCAST_TO_SERIES + podcast.id);
 
-	var series = {
-		id: podcast.id,
-		type: "series",
-		name: podcast.title,
-		poster: podcast.thumbnail,
-		//genres: genresData.getGenresStringsFromArray(podcast.genre_ids),
-		posterShape: "landscape",
-		background: podcast.image,
-		logo: constants.PODCAST_LOGO,
-		description: podcast.description,
-		releaseInfo: (new Date(podcast.earliest_pub_date_ms)).getFullYear() + "-" + (new Date(podcast.latest_pub_date_ms)).getFullYear(),
-		director: [podcast.publisher],
-		//imdbRating: 10,
-		//dvdRelease: "",
-		released: (new Date(podcast.earliest_pub_date_ms)).toISOString(),
-		inTheaters: true,
-		//videos: episodesAsVideos.asArray,
+    var series = {
+        id: podcast.id,
+        type: "series",
+        name: podcast.title,
+        poster: podcast.thumbnail,
+        //genres: genresData.getGenresStringsFromArray(podcast.genre_ids),
+        genres: generateBasicGenres(podcast),
+        posterShape: "landscape",
+        background: podcast.image,
+        logo: constants.PODCAST_LOGO,
+        description: podcast.description,
+        releaseInfo: generateReleaseInfo(podcast.earliest_pub_date_ms, podcast.latest_pub_date_ms),
+        director: [podcast.publisher],
+        //imdbRating: 10,
+        //dvdRelease: "",
+        released: (new Date(podcast.earliest_pub_date_ms)).toISOString(),
+        inTheaters: true,
+        //videos: episodesAsVideos.asArray,
         certification: constants.API_CONSTANTS.DEFAULT_CERTIFICATION,
         //runtime = "Last episode length: " + (podcast.episodes[0].audio_length_sec / 60).toFixed(0) + " minutes",
-		language: podcast.language,
-		country: podcast.country,
-		awards: generateAwards(podcast.explicit_content, podcast.is_claimed),
-		website: podcast.website
+        language: podcast.language,
+        country: podcast.country,
+        awards: generateAwards(podcast.explicit_content, podcast.is_claimed),
+        website: podcast.website
     }
-    
+
     // Sets series parameters if there is episodes to the podcast
-    if (podcast.episodes){
+    if (podcast.episodes) {
         series.runtime = "Last episode length: " + (podcast.episodes[0].audio_length_sec / 60).toFixed(0) + " minutes";
         series.genres = genresData.getGenresStringsFromArray(podcast.genre_ids);
 
@@ -94,7 +95,7 @@ function podcastToSeries(podcast){
     return (series);
 }
 
-function podcastsToSerieses(podcasts, simpleGenre){
+function podcastsToSerieses(podcasts, simpleGenre) {
 
     let serieses = {
         asArray: [],
@@ -102,7 +103,7 @@ function podcastsToSerieses(podcasts, simpleGenre){
     };
 
     podcasts.forEach(podcast => {
-        
+
         let currentSeries = podcastToSeries(podcast);
 
         serieses.asArray.push(currentSeries);
@@ -112,32 +113,52 @@ function podcastsToSerieses(podcasts, simpleGenre){
     return (serieses);
 }
 
-function generateAwards(explicit_content, is_claimed){
+function generateAwards(explicit_content, is_claimed) {
 
     let awards = "Explicit Content: X | ";
 
     if (explicit_content) {
-		awards = "Explicit Content: V | "
+        awards = "Explicit Content: V | "
     }
-    
+
     awards += "Claimed: ";
 
-	if (is_claimed) {
-		awards += "V"
-	} else {
-		awards += "X"
+    if (is_claimed) {
+        awards += "V"
+    } else {
+        awards += "X"
     }
 
     return (awards);
 }
 
-function generateGenres(podcastGenres){
+function generateBasicGenres(podcast) {
 
-    // TODO: Get strings by podcasts genres ids
-    return (["drama", "history"])
+    let basicGenres = [constants.API_CONSTANTS.STREAMS_TITLES.LISTEN_NOTES_STREAM_TITLE,
+        "Number of Episodes: " + podcast.total_episodes,
+        "Country: " + podcast.country, "Language: " + podcast.language
+    ]
+
+    if (podcast.explicit_content) basicGenres.push("*Include Explicit Contact")
+
+    return (basicGenres);
 }
 
-function podcastToSeriesVideo(podcast){
+function generateReleaseInfo(oldestEpisodeTime, newestEpisodeTime) {
+
+    // The release info label shows years
+    let oldestEpisodeYear = (new Date(oldestEpisodeTime)).getFullYear();
+    let newestEpisosdeYear = (new Date(newestEpisodeTime)).getFullYear();
+    let releaseInfo = oldestEpisodeYear + "-" + newestEpisosdeYear;
+    if (oldestEpisodeYear == newestEpisosdeYear) {
+
+        releaseInfo = oldestEpisodeYear;
+    }
+
+    return (releaseInfo);
+}
+
+function podcastToSeriesVideo(podcast) {
 
     return ({
         id: "tt0108778:1:1",
@@ -152,4 +173,9 @@ function podcastToSeriesVideo(podcast){
     })
 };
 
-module.exports = {episodesToVideos, podcastsToSerieses, podcastToSeries, podcastToSeriesVideo};
+module.exports = {
+    episodesToVideos,
+    podcastsToSerieses,
+    podcastToSeries,
+    podcastToSeriesVideo
+};
