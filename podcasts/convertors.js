61ea31ac5a8d555880951e93d9023346b5c0fe11
NM <50276767+NivM1@users.noreply.github.com>
2019-05-11 04:57:41 +0300

    a

--- podcasts/convertors.js
+++ podcasts/convertors.js
@@ -50,8 +50,7 @@ function episodesToVideos(episodes) {
     return (videos);
 }
 
-<<<<<<< HEAD
-function podcastToSeries(podcast) {
+async function podcastToSeries(podcast) {
 
     logger.trace(constants.LOG_MESSAGES.START_CONVERT_PODCAST_TO_SERIES + podcast.id);
 
@@ -83,40 +82,7 @@ function podcastToSeries(podcast) {
 
     // Sets series parameters if there is episodes to the podcast
     if (podcast.episodes) {
-=======
-async function podcastToSeries(podcast){
-
-    logger.trace(constants.LOG_MESSAGES.START_CONVERT_PODCAST_TO_SERIES + podcast.id);
-
-	let series = {
-		id: podcast.id,
-		type: "series",
-		name: podcast.title,
-		poster: podcast.thumbnail,
-		//genres: genresData.getGenresStringsFromArray(podcast.genre_ids),
-		posterShape: "landscape",
-		background: podcast.image,
-		logo: constants.PODCAST_LOGO,
-		description: podcast.description,
-		releaseInfo: (new Date(podcast.earliest_pub_date_ms)).getFullYear() + "-" + (new Date(podcast.latest_pub_date_ms)).getFullYear(),
-		director: [podcast.publisher],
-		//imdbRating: 10,
-		//dvdRelease: "",
-		released: (new Date(podcast.earliest_pub_date_ms)).toISOString(),
-		inTheaters: true,
-		//videos: episodesAsVideos.asArray,
-        certification: constants.API_CONSTANTS.DEFAULT_CERTIFICATION,
-        //runtime = "Last episode length: " + (podcast.episodes[0].audio_length_sec / 60).toFixed(0) + " minutes",
-		language: podcast.language,
-		country: podcast.country,
-		awards: generateAwards(podcast.explicit_content, podcast.is_claimed),
-		website: podcast.website
-    };
-    
-    // Sets series parameters if there is episodes to the podcast
-    if (podcast.episodes){
         const allEpisodes = await podcastsData.getAllEpisodesForPodcast(podcast);
->>>>>>> 3fa974ed629a5d196fc32bf28ed2a5dca7af8071
         series.runtime = "Last episode length: " + (podcast.episodes[0].audio_length_sec / 60).toFixed(0) + " minutes";
         series.genres = genresData.getGenresStringsFromArray(podcast.genre_ids);
 
@@ -130,25 +96,15 @@ async function podcastToSeries(podcast){
     return series;
 }
 
-<<<<<<< HEAD
-function podcastsToSerieses(podcasts, simpleGenre) {
-=======
 async function podcastsToSerieses(podcasts, simpleGenre){
->>>>>>> 3fa974ed629a5d196fc32bf28ed2a5dca7af8071
 
     let serieses = {
         asArray: [],
         asObjectById: {}
     };
 
-<<<<<<< HEAD
-    podcasts.forEach(podcast => {
-
-        let currentSeries = podcastToSeries(podcast);
-=======
     for (let i=0;i<podcasts.length;i++){
         let currentSeries = await podcastToSeries(podcasts[i]);
->>>>>>> 3fa974ed629a5d196fc32bf28ed2a5dca7af8071
 
         serieses.asArray.push(currentSeries);
         serieses.asObjectById[podcasts[i].id] = currentSeries;
