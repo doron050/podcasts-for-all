7b450110e7c899c43eaad8fe87f3aec6dde76229
Doron Eli <doron.eli@naturalint.com>
2019-05-13 21:37:55 +0300

    add landing page

--- serverless.js
+++ serverless.js
@@ -1,6 +1,15 @@
 const { getRouter } = require("stremio-addon-sdk");
 const addonInterface = require("./addon");
+const landingTemplate = require("./landingTemplate");
 const router = getRouter(addonInterface);
+
+// landing page
+const landingHTML = landingTemplate(addonInterface.manifest);
+app.get('/', (_, res) => {
+    res.setHeader('content-type', 'text/html');
+    res.end(landingHTML);
+});
+
 module.exports = function(req, res) {
     router(req, res, function() {
         res.statusCode = 404;
