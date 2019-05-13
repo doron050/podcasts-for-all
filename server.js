260aa2a878f7240fc7c7cab733f69dc81564dccb
Doron Eli <doron.eli@naturalint.com>
2019-05-13 20:42:34 +0300

    fix now.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const { serveHTTP, publishToCentral } = require("stremio-addon-sdk");
 const addonInterface = require("./addon");
-serveHTTP(addonInterface, { port: 56960, static: "/static" });
+serveHTTP(addonInterface, { port: 56960 });
 
 // when you've deployed your addon, un-comment this line
 // publishToCentral("https://my-addon.awesome/manifest.json")
