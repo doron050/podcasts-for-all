643e580283511d1c694c2110589ae132f0742787
NM <50276767+NivM1@users.noreply.github.com>
2019-05-14 01:20:28 +0300

    try redirect

--- serverless.js
+++ serverless.js
@@ -1,17 +1,28 @@
-const { getRouter } = require("stremio-addon-sdk");
+const {
+    getRouter
+} = require("stremio-addon-sdk");
 const addonInterface = require("./addon");
 const landingTemplate = require("./landingTemplate");
 const router = getRouter(addonInterface);
 
 // landing page
 const landingHTML = landingTemplate(addonInterface.manifest);
+
+router.get('/redirectToAddonInstall', (_, res) => {
+
+    response.writeHead(301, {
+        Location: 'stremio://podcasts-for-all.ga/manifest.json'
+    });
+    response.end();
+});
+
 router.get('/', (_, res) => {
     res.setHeader('content-type', 'text/html');
     res.end(landingHTML);
 });
 
-module.exports = function(req, res) {
-    router(req, res, function() {
+module.exports = function (req, res) {
+    router(req, res, function () {
         res.statusCode = 404;
         res.end();
     });
